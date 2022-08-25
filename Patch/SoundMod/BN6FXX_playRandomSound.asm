32127f75ecb63536d6a2a8cca4e9a0b6fb086388
Tekina2 <47544937+Tekina2@users.noreply.github.com>
2022-08-25 02:55:00 -0400

    Update BN6FXX_playRandomSound.asm

--- Patch/SoundMod/BN6FXX_playRandomSound.asm
+++ Patch/SoundMod/BN6FXX_playRandomSound.asm
@@ -17,29 +17,48 @@ playRandomSound:
   isBossBattleSong:
     cmp r0, 0x16  // is boss battle song?
     bne isInternetSong
+// for BN6FXX
+.open "BN6FXX_SoundMod.gba", "BN6FXX_SoundMod_asm.gba", 0x08000000
+
+.get
+
+.org 0x08070340 // free? space
+
+playRandomSound:
+  push r14 
+  cmp r0,0x15  // is virus battle song? 
+  bne isBossBattleSong
+
+  isVirusSong:
+    ldr r0, =virusPlayList
+    b pickRandomSong
+
+  isBossBattleSong:
+    cmp r0,0x16  // is boss battle song? 
+    bne isInternetSong
     ldr r0, = bossPlayList
     b pickRandomSong
 
   isInternetSong:
-    cmp r0, 0x13  // is internet song?
+    cmp r0,0x13  // is internet song? 
     bne originalProcessing
     ldr r0, = internetPlayList
 
   pickRandomSong:
     ldr r3, =0x2001120  // fluctuating value
-    ldrb r3, [r3, 0h]
+    ldrb r3 , [ r3 , 0h ]  
     lsr r3, r3, 4 // get 4bit value to select from playList
-    ldrb r0, [r0, r3]
+    ldrb r0 , [ r0 , r3 ] 
 
   originalProcessing:
-    lsl r0, r0, 0x10
+    lsl r0, r0,0x10 
     ldr r3, =0x8158278
 
-  pop r15
+  pop r15 
 
 .pool
 virusPlayList:
-  .byte 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28, 28  // virus battle songs
+  .byte 21, 38, 42, 47, 52, 56, 60, 21, 38, 42, 47, 52, 56, 60, 21, 38  // virus battle songs
 
 bossPlayList:
   .byte 22, 34, 39, 43, 45, 48, 50, 53, 57, 61, 62, 22, 34, 39, 43, 45 // boss battle songs
